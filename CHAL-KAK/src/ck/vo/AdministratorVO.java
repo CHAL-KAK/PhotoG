1b0fc97d4cbe5b423f207d53da9788a561205f24
uni3621 <uni3621@naver.com>
2020-08-03 12:29:50 +0900

    vo 파일 추가

---
+++ CHAL-KAK/src/ck/vo/AdministratorVO.java
@@ -0,0 +1,28 @@
+package ck.vo;
+
+public class AdministratorVO {
+	private String id;
+	private String password;
+
+	public AdministratorVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
---
+++ CHAL-KAK/src/ck/vo/MapVO.java
@@ -0,0 +1,45 @@
+package ck.vo;
+
+public class MapVO {
+	private int map_seq;
+	private int lat;
+	private int lon;
+	private String path;
+	private String p_id;
+	public MapVO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getMap_seq() {
+		return map_seq;
+	}
+	public void setMap_seq(int map_seq) {
+		this.map_seq = map_seq;
+	}
+	public int getLat() {
+		return lat;
+	}
+	public void setLat(int lat) {
+		this.lat = lat;
+	}
+	public int getLon() {
+		return lon;
+	}
+	public void setLon(int lon) {
+		this.lon = lon;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getP_id() {
+		return p_id;
+	}
+	public void setP_id(String p_id) {
+		this.p_id = p_id;
+	}
+	
+	
+}
